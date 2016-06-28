6406e8e5e42d55ce398c4f9f1aceb0abce835cbb
bonefold <erin@bonefold.com>
2016-06-28 13:42:57 -0400

    new rails app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "92632d9df76405e419af70fe06de5094"
+  config.secret = "0a8e391f40c081845bb25103cb32de46"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
